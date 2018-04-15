38b2989abf748d44c2c6a7f5c31b2747f3e7156d
Aline Cardoso Alexandrte <32268771+alinealexandre@users.noreply.github.com>
2018-04-15 19:21:28 -0300

    Add files via upload

---
+++ rodape.php
@@ -0,0 +1,50 @@
+<br>
+<hr>
+ <div class="ui inverted vertical footer segment">
+    <div class="ui center aligned container">
+      <div class="ui stackable inverted divided grid">
+        <div class="three wide column centered">
+          <h4 class="ui inverted header">Developers</h4>
+          <div class="ui inverted link list">
+            <a class="item">Aline Soster</a>
+            <a class="item">Felipe Passig</a>
+            <a class="item">Mateus Cardoso</a>
+          </div>
+        </div>
+        
+        <div class="seven wide column">
+          <h4 class="ui inverted header">Open World</h4>
+          <p>Your site for consults of games.</p>
+        </div>
+      </div>
+      <br>
+      <br>
+      <a href="https://pt-br.facebook.com/">
+      <button class="blue ui facebook button">
+  <i class="facebook icon"></i>
+  Facebook
+</button>
+</a>
+<a href="https://twitter.com/login?lang=pt">
+<button class="blue ui twitter button">
+  <i class="twitter icon"></i>
+  Twitter
+</button>
+</a>
+<a href="https://www.instagram.com/?hl=pt-br">
+<button class=" blue ui instagram button">
+  <i class="instagram icon"></i>
+  Instagram
+</button>
+</a>
+<br>
+      <div class="ui horizontal inverted small divided link list">
+        <a class="item" href="#">Site Map</a>
+        <a class="item" href="#">Contact Us</a>
+        <a class="item" href="#">Terms and Conditions</a>
+        <a class="item" href="#">Privacy Policy</a>
+      </div>
+    </div>
+</div>
+</body>
+</html>
