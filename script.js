8933accb8abeb8d9dbb96a3d21151ff8aecbee10
Aline Cardoso Alexandrte <32268771+alinealexandre@users.noreply.github.com>
2018-04-15 13:26:16 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+$(document).ready(function(){
+
+$('.a1').click(function(){
+			var id;
+		id=	$(this).attr('id');
+		$('.'+id).toggleClass('escondido');
+	})	
+
+$('.a2').click(function(){
+			var id;
+		id=	$(this).attr('id');
+		$('.'+id).toggleClass('escondido');
+	})
+
+$('.a3').click(function(){
+			var id;
+		id=	$(this).attr('id');
+		$('.'+id).toggleClass('escondido');
+	})
+
+
+$('.a4').click(function(){
+			var id;
+		id=	$(this).attr('id');
+		$('.'+id).toggleClass('escondido');
+	})
+
+$('.a5').click(function(){
+			var id;
+		id=	$(this).attr('id');
+		$('.'+id).toggleClass('escondido');
+	})
+
+$('.a6').click(function(){
+			var id;
+		id=	$(this).attr('id');
+		$('.'+id).toggleClass('escondido');
+	})
+
+
+
+
+$('.special.cards .image').dimmer({
+  on: 'hover'
+});
+})
